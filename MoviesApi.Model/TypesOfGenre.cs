b4ac3d66581542380150678bb8cfc03008b95329
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-09-16 13:30:07 +0200

    Add enums for models properties

---
+++ MoviesApi.Model/TypesOfGenre.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApi.Model
+{
+    public enum TypesOfGenre
+    {
+        THRILLER,
+        FANTASY,
+        DRAMA,
+        COMEDY
+    }
+}
---
+++ MoviesApi.Model/TypesOfSex.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MoviesApi.Model
+{
+    public enum TypeOfSex
+    {
+        MALE,
+        FEMALE
+    }
+}
