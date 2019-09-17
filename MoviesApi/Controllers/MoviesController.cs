484c475ad5c483f9a173d2a4e949092dcdcde81e
Patryk Lichwala <lichwalapatryk@gmail.com>
2019-09-17 10:11:10 +0200

    Add movie controller for api

---
+++ MoviesApi/Controllers/MoviesController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MoviesApi.AccessLayer;
+using MoviesApi.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MoviesApi
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MoviesController : ControllerBase
+    {
+        private readonly MoviesDBEntities _context;
+
+        public MoviesController(MoviesDBEntities context)
+        {
+           
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Country>>> GetTodoItems()
+        {
+            return await _context.Countries.ToListAsync();
+        }
+    }
+}
