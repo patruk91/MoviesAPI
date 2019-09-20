8d572e2fa66583a33073bc9657fed3f2f856cea9
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-09-20 09:27:20 +0200

    Add personController to get all people and persob by id

--- MoviesApi.Model/DTO/PersonDTO.cs
+++ MoviesApi.Model/DTO/PersonDTO.cs
@@ -2,15 +2,15 @@
 
 namespace MoviesApi.Model.DTO
 {
-    class PersonDTO
+    public class PersonDTO
     {
-        public int Id { get; private set; }
+        public int Id { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         public TypeOfPeople Type { get; set; }
         public DateTime DateOfBirth { get; set; }
         public DateTime DateOfDeath { get; set; }
         public TypeOfSex Sex { get; set; }
-        public Country Country { get; set; }
+        public int CountryId { get; set; }
     }
 }
---
+++ MoviesApi/Controllers/PersonController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MoviesApi.AccessLayer;
+using MoviesApi.Model.DTO;
+using MoviesApi.Model;
+using MoviesApi.Model.DbModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace MoviesApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonController : ControllerBase
+    {
+        private readonly MoviesDBEntities _context;
+
+        public PersonController(MoviesDBEntities context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PersonDTO>>> GetPerson()
+        {
+            return await _context.People
+                .Select(x => new PersonDTO
+                {
+                    Id = x.Id,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Type = x.Type,
+                    DateOfBirth = x.DateOfBirth,
+                    DateOfDeath = x.DateOfDeath,
+                    Sex = x.Sex,
+                    CountryId = x.Country.Id
+                }).ToListAsync();
+        }
+
+        [Route("{id:int}")]
+        [HttpGet]
+        public async Task<ActionResult<PersonDTO>> GetMovie(int id)
+        {
+            var personDTO = await _context.People
+                .Where(x => x.Id == id)
+                .Select(x => new PersonDTO
+                {
+                    Id = x.Id,
+                    FirstName = x.FirstName,
+                    LastName = x.LastName,
+                    Type = x.Type,
+                    DateOfBirth = x.DateOfBirth,
+                    DateOfDeath = x.DateOfDeath,
+                    Sex = x.Sex,
+                    CountryId = x.Country.Id
+                }).FirstOrDefaultAsync();
+            if (personDTO == null)
+            {
+                return NotFound();
+            }
+            return personDTO;
+        }
+    }
+}
