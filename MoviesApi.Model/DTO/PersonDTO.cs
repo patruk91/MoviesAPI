7ebe7afad0a14dab5888e2c0383b13cc3d14634a
CzajaKrzysztof <krzysiekczaja@gmail.com>
2019-09-20 08:57:24 +0200

    Add personDTO model

---
+++ MoviesApi.Model/DTO/PersonDTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace MoviesApi.Model.DTO
+{
+    class PersonDTO
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public TypeOfPeople Type { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfDeath { get; set; }
+        public TypeOfSex Sex { get; set; }
+        public Country Country { get; set; }
+    }
+}
